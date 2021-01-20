2df7b58ce62c4ea85c07cd6071b898c9ec29cb60
Neil Sainsbury <neil.sainsbury@gmail.com>
2021-01-20 15:57:49 +1000

    Completed Day 9 (Part 1)

---
+++ src/Day09a.kt
@@ -0,0 +1,36 @@
+import java.io.File
+import java.math.BigInteger
+import java.util.*
+
+const val WINDOW_SIZE = 25
+
+fun main() {
+    val window: Queue<BigInteger> = LinkedList()
+
+    val nums = File("data/day09_input.txt")
+        .readLines()
+        .map { it.toBigInteger() }
+
+    window.addAll(nums.take(WINDOW_SIZE))
+    for (n in nums.drop(WINDOW_SIZE)) {
+        if (!window.canSumTo(n)) {
+            println("can't make $n from the current window")
+            break
+        }
+
+        window.remove()
+        window.add(n)
+    }
+}
+
+private fun Collection<BigInteger>.canSumTo(n: BigInteger): Boolean {
+    val required = mutableSetOf<BigInteger>()
+
+    for (curr in this) {
+        if (curr in required)
+            return true
+        required.add(n - curr)
+    }
+
+    return false
+}
